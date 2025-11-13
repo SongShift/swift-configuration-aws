b1c8b454bf6e91b0dd163bb8c9f586c11ac82331
Ben Rosen <ben@songshift.com>
2025-11-13 16:03:20 +0000

    Implement polling, watching, snapshots, much more properly

---
+++ Sources/ConfigurationAWS/AWSSecretsManagerProvider+ServiceLifecycle.swift
@@ -0,0 +1,37 @@
+//
+//  AWSSecretsManagerProvider+ServiceLifecycle.swift
+//  swift-configuration-aws
+//
+//  Created by Ben on 11/13/25.
+//  Copyright © 2025 SongShift, LLC. All rights reserved.
+//
+
+import ServiceLifecycle
+import AsyncAlgorithms
+
+#if canImport(FoundationEssentials)
+import FoundationEssentials
+#else
+import Foundation
+#endif
+
+// MARK: - Swift Service Lifecycle
+extension AWSSecretsManagerProvider: Service {
+    
+    public func run() async throws {
+        guard let _pollingInterval else { return }
+        for try await _ in AsyncTimerSequence(interval: Duration.seconds(1), clock: .continuous).cancelOnGracefulShutdown() {
+            do {
+                try await withThrowingDiscardingTaskGroup { taskGroup in
+                    for prefetchSecretName in _prefetchSecretNames {
+                        taskGroup.addTask {
+                            try await self.reloadSecretIfNeeded(secretName: prefetchSecretName, overrideCacheTTL: true)
+                        }
+                    }
+                }
+            } catch {
+                print("Failed while polling: \(error)")
+            }
+        }
+    }
+}
